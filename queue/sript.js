a1b44788df75e3086e5742fd438ee9c708faaa31
adzarayanmolejon <adzmolejon@yahoo.com>
2024-04-19 15:05:53 +0800

    commit

--- queue/sript.js
+++ queue/sript.js
@@ -35,6 +35,11 @@ let singleQueueNumber = 1;
 let multipleQueueNumber = 1;
 let priorityQueueNumber = 1;
 
+// Rest of your JavaScript code using these variables (e.g., nextNumber function)
+function nextNumber(queueType) {
+  // ... (code using singleQueueNumber, multipleQueueNumber, priorityQueueNumber)
+}
+
 function nextNumber(queueType) {
     let newNumber;
     switch (queueType) {
@@ -65,4 +70,27 @@ function nextNumber(queueType) {
     return newNumber;
 }
 
-  
+function printNumber(queueType) {
+    let queueNumber;
+    switch (queueType) {
+        case 'single':
+            queueNumber = singleQueueNumber - 1; // Print the last generated number
+            break;
+        case 'multiple':
+            queueNumber = multipleQueueNumber - 1; // Print the last generated number
+            break;
+        case 'priority':
+            queueNumber = priorityQueueNumber - 1; // Print the last generated number
+            break;
+        default:
+            console.error('Invalid queue type');
+            return;
+    }
+    
+    // Print the queue number
+    let printWindow = window.open('', '_blank');
+    printWindow.document.open();
+    printWindow.document.write(`<h1>Queue Number: ${queueNumber}</h1>`);
+    printWindow.document.close();
+    printWindow.print();
+}
