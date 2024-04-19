ac0fcf028f23bc16f32de3ca8c116bf82515022b
adzarayanmolejon <adzmolejon@yahoo.com>
2024-04-19 13:31:05 +0800

    commit

--- queue/sript.js
+++ queue/sript.js
@@ -52,3 +52,36 @@ function nextNumber(queueType) {
     console.log('Current queue:', queue);
     // Your code here
 }
+let singleQueueNumber = 1;
+let multipleQueueNumber = 1;
+let priorityQueueNumber = 1;
+
+function nextNumber(queueType) {
+    let newNumber;
+    switch (queueType) {
+        case 'single':
+            newNumber = singleQueueNumber;
+            singleQueueNumber++;
+            document.getElementById('singleQueueNumber').innerHTML = newNumber;
+            break;
+        case 'multiple':
+            newNumber = multipleQueueNumber;
+            multipleQueueNumber++;
+            document.getElementById('multipleQueueNumber').innerHTML = newNumber;
+            break;
+        case 'priority':
+            newNumber = priorityQueueNumber;
+            priorityQueueNumber++;
+            document.getElementById('priorityQueueNumber').innerHTML = newNumber;
+            break;
+        default:
+            console.error('Invalid queue type');
+            return;
+    }
+    return newNumber;
+}
+
+// Example usage
+// nextNumber('single');
+// nextNumber('multiple');
+// nextNumber('priority');
