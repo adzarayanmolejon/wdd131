66a6d5de45c8673d863b8c0ae3cb4f1553d4ce98
adzarayanmolejon <adzmolejon@yahoo.com>
2024-04-19 15:24:47 +0800

    commit

--- queue/script.js
+++ queue/script.js
@@ -34,11 +34,11 @@ function nextNumber(queueType) {
 let singleQueueNumber = 1;
 let multipleQueueNumber = 1;
 let priorityQueueNumber = 1;
-
-// Rest of your JavaScript code using these variables (e.g., nextNumber function)
-function nextNumber(queueType) {
-  // ... (code using singleQueueNumber, multipleQueueNumber, priorityQueueNumber)
-}
+let printedNumbers = {
+    single: [],
+    multiple: [],
+    priority: []
+};
 
 function nextNumber(queueType) {
     let newNumber;
@@ -75,12 +75,15 @@ function printNumber(queueType) {
     switch (queueType) {
         case 'single':
             queueNumber = singleQueueNumber - 1; // Print the last generated number
+            printedNumbers.single.push(queueNumber);
             break;
         case 'multiple':
             queueNumber = multipleQueueNumber - 1; // Print the last generated number
+            printedNumbers.multiple.push(queueNumber);
             break;
         case 'priority':
             queueNumber = priorityQueueNumber - 1; // Print the last generated number
+            printedNumbers.priority.push(queueNumber);
             break;
         default:
             console.error('Invalid queue type');
@@ -93,4 +96,14 @@ function printNumber(queueType) {
     printWindow.document.write(`<h1>Queue Number: ${queueNumber}</h1>`);
     printWindow.document.close();
     printWindow.print();
+}
+
+function callNumber(queueType) {
+    let queueNumbers = printedNumbers[queueType];
+    if (queueNumbers.length > 0) {
+        let nextNumber = queueNumbers.shift();
+        alert(`Calling ${queueType} queue number: ${nextNumber}`);
+    } else {
+        alert(`No more numbers to call for ${queueType} queue`);
+    }
 }
