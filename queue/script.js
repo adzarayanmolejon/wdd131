d147ddd4ab77b46f716560288007015c0bb3ab7b
adzarayanmolejon <adzmolejon@yahoo.com>
2024-04-22 09:04:22 +0800

    commit

--- queue/script.js
+++ queue/script.js
@@ -1,109 +1,51 @@
-let singleQueueNumber = 1;
-let multipleQueueNumber = 1;
-let priorityQueueNumber = 1;
+let singleQueue = [];
+let multipleQueue = [];
+let priorityQueue = [];
+let queueNumber = 1;
 
-function nextNumber(queueType) {
-    let newNumber;
+document.getElementById('queueForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let name = document.getElementById('name').value;
+    let queueType = 'single'; // Assume single queue for now
+    assignQueueNumber(name, queueType);
+});
+
+function assignQueueNumber(name, queueType) {
+    let newNumber = queueNumber++;
     switch (queueType) {
         case 'single':
-            newNumber = singleQueueNumber;
-            singleQueueNumber++;
-            document.getElementById('singleQueueNumber').innerHTML = newNumber;
+            singleQueue.push({ number: newNumber, name: name });
             break;
         case 'multiple':
-            newNumber = multipleQueueNumber;
-            multipleQueueNumber++;
-            document.getElementById('multipleQueueNumber').innerHTML = newNumber;
+            multipleQueue.push({ number: newNumber, name: name });
             break;
         case 'priority':
-            newNumber = priorityQueueNumber;
-            priorityQueueNumber++;
-            document.getElementById('priorityQueueNumber').innerHTML = newNumber;
+            priorityQueue.push({ number: newNumber, name: name });
             break;
         default:
             console.error('Invalid queue type');
             return;
     }
-    return newNumber;
+    displayVisualCallout(newNumber);
 }
 
-// Example usage
-// nextNumber('single');
-// nextNumber('multiple');
-// nextNumber('priority');
-let singleQueueNumber = 1;
-let multipleQueueNumber = 1;
-let priorityQueueNumber = 1;
-let printedNumbers = {
-    single: [],
-    multiple: [],
-    priority: []
-};
+function displayVisualCallout(number) {
+    document.getElementById('visualCallout').innerText = `Calling number ${number}`;
+}
 
-function nextNumber(queueType) {
-    let newNumber;
+function resetQueue(queueType) {
     switch (queueType) {
         case 'single':
-            newNumber = singleQueueNumber;
-            singleQueueNumber++;
-            document.getElementById('singleQueueNumber').innerHTML = newNumber;
+            singleQueue = [];
             break;
         case 'multiple':
-            newNumber = multipleQueueNumber;
-            multipleQueueNumber++;
-            document.getElementById('multipleQueueNumber').innerHTML = newNumber;
+            multipleQueue = [];
             break;
         case 'priority':
-            newNumber = priorityQueueNumber;
-            priorityQueueNumber++;
-            document.getElementById('priorityQueueNumber').innerHTML = newNumber;
+            priorityQueue = [];
             break;
         default:
             console.error('Invalid queue type');
             return;
-    }
-    
-    // Play audio
-    let audio = document.getElementById('audio');
-    audio.play();
-
-    return newNumber;
-}
-
-function printNumber(queueType) {
-    let queueNumber;
-    switch (queueType) {
-        case 'single':
-            queueNumber = singleQueueNumber - 1; // Print the last generated number
-            printedNumbers.single.push(queueNumber);
-            break;
-        case 'multiple':
-            queueNumber = multipleQueueNumber - 1; // Print the last generated number
-            printedNumbers.multiple.push(queueNumber);
-            break;
-        case 'priority':
-            queueNumber = priorityQueueNumber - 1; // Print the last generated number
-            printedNumbers.priority.push(queueNumber);
-            break;
-        default:
-            console.error('Invalid queue type');
-            return;
-    }
-    
-    // Print the queue number
-    let printWindow = window.open('', '_blank');
-    printWindow.document.open();
-    printWindow.document.write(`<h1>Queue Number: ${queueNumber}</h1>`);
-    printWindow.document.close();
-    printWindow.print();
-}
-
-function callNumber(queueType) {
-    let queueNumbers = printedNumbers[queueType];
-    if (queueNumbers.length > 0) {
-        let nextNumber = queueNumbers.shift();
-        alert(`Calling ${queueType} queue number: ${nextNumber}`);
-    } else {
-        alert(`No more numbers to call for ${queueType} queue`);
     }
 }
