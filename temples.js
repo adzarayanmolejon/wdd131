85cce7e28926a766557cbb48118ccab94e8479f9
adzarayanmolejon <adzmolejon@yahoo.com>
2024-05-04 10:08:50 +0800

    commit

--- temples.js
+++ temples.js
@@ -1,2 +1,11 @@
 document.getElementById('lastModified').textContent = document.lastModified;
 
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+var lastModified = new Date(document.lastModified);
+var formattedDate = lastModified.toLocaleDateString();
+var formattedTime = lastModified.toLocaleTimeString();
+
+document.getElementById("lastModified").textContent = "Last Modified: " + formattedDate + " " + formattedTime;
+
+
