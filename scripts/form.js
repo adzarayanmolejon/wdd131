3356a4d519d9e0919ad82ca15ab5bb5aef5eee06
adzarayanmolejon <adzmolejon@yahoo.com>
2024-05-25 21:53:56 +0800

    commit

--- scripts/form.js
+++ scripts/form.js
@@ -28,10 +28,9 @@ const products = [
 
   document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-var lastModified = new Date(document.lastModified);
-var formattedDate = lastModified.toLocaleDateString();
-var formattedTime = lastModified.toLocaleTimeString();
-
-document.getElementById("lastModified").textContent = "Last Modified: " + formattedDate + " " + formattedTime;
-
+  var lastModified = new Date(document.lastModified);
+  var formattedDate = lastModified.toLocaleDateString();
+  var formattedTime = lastModified.toLocaleTimeString();
+  
+  document.getElementById("lastModified").textContent = "Last Modified: " + formattedDate + " " + formattedTime;
   
