c49d46ea8dcaca7a7145ec5612c89c0730f99f11
adzarayanmolejon <adzmolejon@yahoo.com>
2024-05-25 22:05:41 +0800

    commit

--- scripts/form.js
+++ scripts/form.js
@@ -1,36 +1,36 @@
 const products = [
-    {
-      id: fc-1888,
-      name: "flux capacitor",
-      avg-rating: 4.5
-    },
-    {
-      id: fc-2050,
-      name: "power laces",
-      averagerating: 4.7
-    },
-    {
-      id: fs-1987,
-      name: "time circuits",
-      averagerating: 3.5
-    },
-    {
-      id: ac-2000,
-      name: "low voltage reactor",
-      averagerating: 3.9
-    },
-    {
-      id: jj-1969,
-      name: "warp equalizer",
-      averagerating: 5.0
-    }
-  ];
+  {
+    'id': 'fc-1888',
+    'name': 'flux capacitor',
+    'avgRating': 4.5
+  },
+  {
+    'id': 'fc-2050',
+    'name': 'power laces',
+    'avgRating': 4.7
+  },
+  {
+    'id': 'fs-1987',
+    'name': 'time circuits',
+    'avgRating': 3.5
+  },
+  {
+    'id': 'ac-2000',
+    'name': 'low voltage reactor',
+    'avgRating': 3.9
+  },
+  {
+    'id': 'jj-1969',
+    'name': 'warp equalizer',
+    'avgRating': 5.0
+  }
+];
+ //date:modification
+document.getElementById("currentyear").textContent = new Date().getFullYear();
 
-  document.getElementById("currentyear").textContent = new Date().getFullYear();
+var lastModified = new Date(document.lastModified);
+var formattedDate = lastModified.toLocaleDateString();
+var formattedTime = lastModified.toLocaleTimeString();
 
-  var lastModified = new Date(document.lastModified);
-  var formattedDate = lastModified.toLocaleDateString();
-  var formattedTime = lastModified.toLocaleTimeString();
-  
-  document.getElementById("lastModified").textContent = "Last Modified: " + formattedDate + " " + formattedTime;
-  
+document.getElementById("lastModified").textContent = "Last Modified: " + formattedDate + " " + formattedTime;
+ 
