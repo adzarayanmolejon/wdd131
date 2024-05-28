096a5745cf4e379d07633cb7226bb7a21197f8fc
adzarayanmolejon <adzmolejon@yahoo.com>
2024-05-29 05:55:10 +0800

    commit

---
+++ project/scripts/product.js
@@ -0,0 +1,8 @@
+//date:modification
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+var lastModified = new Date(document.lastModified);
+var formattedDate = lastModified.toLocaleDateString();
+var formattedTime = lastModified.toLocaleTimeString();
+
+document.getElementById("lastModified").textContent = "Last Modified: " + formattedDate + " " + formattedTime;
