514191629927dfb7f042a877507b54ee47ff6bd2
adzarayanmolejon <adzmolejon@yahoo.com>
2024-05-29 08:50:29 +0800

    commit

--- project/scripts/product.js
+++ project/scripts/product.js
@@ -6,3 +6,10 @@ var formattedDate = lastModified.toLocaleDateString();
 var formattedTime = lastModified.toLocaleTimeString();
 
 document.getElementById("lastModified").textContent = "Last Modified: " + formattedDate + " " + formattedTime;
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
