d9e62dca1bd56bf56639db4524f0c10d5e79b65e
adzarayanmolejon <adzmolejon@yahoo.com>
2024-05-30 21:18:27 +0800

    commit

--- project/scripts/product.js
+++ project/scripts/product.js
@@ -13,3 +13,22 @@ const navLinks = document.querySelector('.nav-links');
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+
+// scripts/about.js
+
+document.addEventListener("DOMContentLoaded", function() {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", () => {
+      navLinks.classList.toggle("nav-active");
+  });
+
+  // Set the current year
+  document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+
+  // Set the last modified date
+  const lastModifiedDate = new Date(document.lastModified);
+  const formattedDate = lastModifiedDate.toLocaleDateString();
+  document.getElementById("lastModified").innerHTML += formattedDate ? formattedDate : "Unknown";
+});
