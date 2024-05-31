06449040667b59df0d01df97704e147ceec4e370
adzarayanmolejon <adzmolejon@yahoo.com>
2024-05-31 13:42:09 +0800

    commit

---
+++ project/scripts/contact.js
@@ -0,0 +1,16 @@
+//date:modification
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+var lastModified = new Date(document.lastModified);
+var formattedDate = lastModified.toLocaleDateString();
+var formattedTime = lastModified.toLocaleTimeString();
+
+document.getElementById("lastModified").textContent = "Last Modified: " + formattedDate + " " + formattedTime;
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
